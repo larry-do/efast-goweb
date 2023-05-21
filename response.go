cceed739217102d9f20a92a700ad5e4548dde161
Larry <annanshakai@gmail.com>
2023-05-22 02:44:19 +0700

    Refactor response methods

--- response.go
+++ response.go
@@ -2,32 +2,46 @@ package goweb
 
 import (
 	"encoding/json"
+	"github.com/rs/zerolog/log"
 	"html/template"
 	"net/http"
 )
 
-func (resp Response) RespondJson(object any) {
-	resp.Header().Set("Content-Type", "application/json")
+func (resp Response) RespondJson(statusCode int, data any) {
+	resp.ContentType("application/json").Code(statusCode).Json(data)
+}
+
+func (resp Response) RespondPlainText(statusCode int, data string) {
+	resp.ContentType("text/plain").Code(statusCode).PlainText(data)
+}
+
+func (resp Response) ContentType(contentType string) Response {
+	resp.Header().Set("Content-Type", contentType)
+	return resp
+}
+
+func (resp Response) Json(object any) {
 	var err = json.NewEncoder(resp).Encode(object)
 	if err != nil {
+		log.Error().Err(err).Msg("")
 		return
 	}
 }
 
-func (resp Response) RespondPlainText(str string) {
-	resp.Header().Set("Content-Type", "text/plain")
+func (resp Response) PlainText(str string) {
 	_, err := resp.Write([]byte(str))
 	if err != nil {
+		log.Error().Err(err).Msg("")
 		return
 	}
 }
 
-func (resp Response) ResponseCode(statusCode int) Response {
+func (resp Response) Code(statusCode int) Response {
 	resp.WriteHeader(statusCode)
 	return resp
 }
 
-func (resp Response) RespondHtmlView(viewTemplate string, model any) (Response, error) {
+func (resp Response) HtmlView(viewTemplate string, model any) (Response, error) {
 	var tmpl = template.Must(template.ParseFiles(viewTemplate))
 	var err = tmpl.Execute(resp, model)
 	if err != nil {
